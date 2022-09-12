76fa4af57d9f1d673cee28da11ef298fa1412360
Marco Borth <marcob@youracs.org>
2022-09-12 09:24:27 -0500

    renderRow function added.

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -20,24 +20,22 @@ class Board extends React.Component {
     );
   }
 
+  renderRow(i) {
+    return(
+      <div className="board-row">
+        {this.renderSquare(i)}
+        {this.renderSquare(i+1)}
+        {this.renderSquare(i+2)}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderRow(0)}
+        {this.renderRow(3)}
+        {this.renderRow(6)}
       </div>
     );
   }
